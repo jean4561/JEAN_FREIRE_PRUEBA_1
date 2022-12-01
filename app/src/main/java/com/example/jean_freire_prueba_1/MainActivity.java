0e41cbd62deb005e00d42188af8993d2b9c99ce2
JeanCarloFreire <jeancarlo4561@gmail.com>
2022-12-01 18:29:10 -0500

    tercer Commit

--- app/src/main/java/com/example/jean_freire_prueba_1/MainActivity.java
+++ app/src/main/java/com/example/jean_freire_prueba_1/MainActivity.java
@@ -3,8 +3,16 @@ package com.example.jean_freire_prueba_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+EditText editTextNombre;
+    EditText editTextApellido;
+    EditText editTextDividendo;
+    EditText editTextDivisor;
+    EditText editTextParteEntera;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
