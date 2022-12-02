bc1c04740e4acfc1c1101717da33067041bc9011
JeanCarloFreire <jeancarlo4561@gmail.com>
2022-12-01 19:11:50 -0500

    nuevo Commit

--- app/src/main/java/com/example/jean_freire_prueba_1/MainActivity.java
+++ app/src/main/java/com/example/jean_freire_prueba_1/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.jean_freire_prueba_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,12 +12,27 @@ EditText editTextNombre;
     EditText editTextDividendo;
     EditText editTextDivisor;
     EditText editTextParteEntera;
-
+    EditText editTextResiduo;
+    EditText editTextInvertido;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void Nombres(View view){
+
+    }
+    public void Dividir(View view){
+    float dividendo =Integer.parseInt(editTextDividendo.toString());
+    float divisor   =Integer.parseInt(editTextDivisor.toString());
+    float resultado =dividendo/divisor;
+
+    }
+    public void Enteros(View view){
+        int dividendo =Integer.parseInt(editTextDividendo.toString());
+        int divisor   =Integer.parseInt(editTextDivisor.toString());
+        int resultado =dividendo/divisor;
     }
 }
---
+++ app/src/main/java/com/example/jean_freire_prueba_1/nombre_apellido.java
@@ -0,0 +1,4 @@
+package com.example.jean_freire_prueba_1;
+
+public class nombre_apellido {
+}
